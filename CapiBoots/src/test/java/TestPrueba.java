3bfb9035bed25f86102b249dee5d08ed79b97741
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2023-03-21 13:02:45 +0100

    Creación
    Creación de repositorio

---
+++ CapiBoots/src/test/java/TestPrueba.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestPrueba {
+    @Test
+    void testAssertNull_Null_ShouldPass(){
+        assertNull(null);
+    }
+
+}
---
+++ CapiBoots/src/test/java/com/example/CapiBoots/CapiBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CapiBoots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapiBootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
