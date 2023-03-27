be1b22cb1541ec1b7b699534adfdaeea18164ef9
CPL <carlos@centropyme.com>
2023-03-27 19:22:21 +0200

    Esqueleto de modelos y controladores

---
+++ CapiBoots/src/main/java/com/example/CapiBoots/controladores/UsuarioCtrl.java
@@ -0,0 +1,33 @@
+package com.example.CapiBoots.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UsuarioCtrl {
+    @GetMapping("")
+    public String inicio(Model modelo) {
+        modelo.addAttribute("titulo", "Página de inicio de relaciones N:M");
+        return "index";
+    }
+
+    @GetMapping("/acceso")
+    public String acceso(Model modelo) {
+        modelo.addAttribute("titulo", "Página de acceso");
+        return "login";
+    }
+
+    @PostMapping("/acceso")
+    public String postAcceso(Model modelo){
+        // Verificar que el usuario existe y entrega lña clave correcta
+        modelo.addAttribute("titulo", "Acceso exitoso");
+        return "accedido";
+    }
+
+    @GetMapping("/registro")
+    public String registro(Model modelo){
+        return "registro";
+    }
+}
---
+++ CapiBoots/src/main/java/com/example/CapiBoots/modelos/Usuario.java
@@ -0,0 +1,23 @@
+package com.example.CapiBoots.modelos;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String usuario;
+    @Column(columnDefinition = "VARCHAR(100)")
+    private String clave;
+}
