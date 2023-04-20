6d8e45e3df69a958746258d9bac64a8de0d01343
dsanfealos <123487285+dsanfealos@users.noreply.github.com>
2023-04-20 16:28:44 +0200

    Arreglo de errores en ContenidosCtrl y Comentarios

--- CapiBoots/src/main/java/com/example/CapiBoots/servicios/ContenidosSrvcImpls.java
+++ CapiBoots/src/main/java/com/example/CapiBoots/servicios/ContenidosSrvcImpls.java
@@ -2,10 +2,14 @@ package com.example.CapiBoots.servicios;
 
 import com.example.CapiBoots.modelos.Contenidos;
 import com.example.CapiBoots.repositorios.ContenidosRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ContenidosSrvcImpls implements ifxContenidosSrvc{
+    @Autowired
     public ContenidosRepositorio contenidoRepo;
     @Override
     public Contenidos guardarContenido() {
