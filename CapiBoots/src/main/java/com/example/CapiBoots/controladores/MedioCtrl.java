ecd10d467a125ecca767f2580a48db3773ce6f01
Karim Rachidi <karim.m.rachidi@gmail.com>
2023-04-20 09:53:07 +0200

    DK-3 Hechos los servicios, controladores (Contenidos, Logro y Medio) corregido

--- CapiBoots/src/main/java/com/example/CapiBoots/controladores/MedioCtrl.java
+++ CapiBoots/src/main/java/com/example/CapiBoots/controladores/MedioCtrl.java
@@ -1,5 +1,6 @@
 package com.example.CapiBoots.controladores;
 
+import com.example.CapiBoots.servicios.MedioSrvcImpls;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +9,26 @@ public class MedioCtrl {
 
     @Controller
 public class medioCtrl {
+        private MedioSrvcImpls medioSrvc;
     @GetMapping("/medios")
     public String Medio(Model modelo) {
         modelo.addAttribute("titulo", "Medios");
         return "Medios";
     }
+    @GetMapping("/guardarDatos")
+        public String guardarDatos(Model modelo) {
+            modelo.addAttribute("guardarDatos", medioSrvc.guardarDatos());
+            return "guardarDatos";
+        }
+    @GetMapping("/eliminarDatos")
+        public String eliminarDatos(Model modelo) {
+            modelo.addAttribute("eliminarDatos", medioSrvc.eliminarDatos());
+            return "eliminarDatos";
+        }
+    @GetMapping("/listaMedio")
+        public String listaMedio(Model modelo) {
+            modelo.addAttribute("listaMedio", medioSrvc.listaMedio());
+            return "listaMedio";
+        }
 }
 }
