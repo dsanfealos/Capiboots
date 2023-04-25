5a0fce4c83903afe7aa2fa120e06af9eebe1e58b
RocioSoriano13 <123320333+RocioSoriano13@users.noreply.github.com>
2023-04-25 17:12:21 +0200

    creacion lista controlador de categorias

--- CapiBoots/src/main/java/com/example/CapiBoots/servicios/CategoriasSrvcImpls.java
+++ CapiBoots/src/main/java/com/example/CapiBoots/servicios/CategoriasSrvcImpls.java
@@ -6,16 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoriasSrvcImpls implements ifxCategoriasSrvc {
     @Autowired
     public CategoriasRepositorios catrepo;
     @Override
-    public Categorias buscaId(Long id) {
-        return catrepo.findById(id).orElse(null);
+    public Optional<Categorias> buscaId(Long id) {
+        return catrepo.findById(id);
     }
     @Override
     public List<Categorias> listaCat() {
         return catrepo.findAll();
+    }
+
+    @Override
+    public Categorias guardar(Categorias cat) {
+        return catrepo.save(cat);
+    }
+
+    public void borrar (Long id) {
+        catrepo.deleteById(id);
     }
 }
