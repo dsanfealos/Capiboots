bba78ad016c8cf0e52c19a72430009d055243587
RocioSoriano13 <123320333+RocioSoriano13@users.noreply.github.com>
2023-04-30 20:31:18 +0200

    se añade plantilla de inicio y se activa controlador

--- CapiBoots/src/main/java/com/example/CapiBoots/controladores/UsuarioCtrl.java
+++ CapiBoots/src/main/java/com/example/CapiBoots/controladores/UsuarioCtrl.java
@@ -23,14 +23,14 @@ public class UsuarioCtrl {
 
 
 
-    /*@GetMapping({"","/"})
+    @GetMapping({"","/"})
     public String inicio(Model modelo) {
 //        List<Contenidos> pdtes = accessSrvc.buscaPendientes(2L);
 //        modelo.addAttribute("pendientes", pdtes);
         modelo.addAttribute("titulo", "Página de inicio de relaciones N:M");
         return "/inicio";
     }
-    */
+
     //Login
     @GetMapping("/home")
     public String Home(Model modelo) {
