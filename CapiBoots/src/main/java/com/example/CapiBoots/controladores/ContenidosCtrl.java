d63ed001ea7245b66cbb84dbb6a8a1850c849527
RocioSoriano13 <123320333+RocioSoriano13@users.noreply.github.com>
2023-05-04 14:14:04 +0200

    commit and push antes del merge y tutoria 04.05

--- CapiBoots/src/main/java/com/example/CapiBoots/controladores/ContenidosCtrl.java
+++ CapiBoots/src/main/java/com/example/CapiBoots/controladores/ContenidosCtrl.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
@@ -41,7 +40,7 @@ public class ContenidosCtrl {
         @GetMapping ("/showbox")
         public String Showbox(Model modelo){
             modelo.addAttribute("titulo", "Showbox");
-            return "Showbox";
+            return "moviebox";
         }
         @GetMapping("/bookbox")
         public String bookbox(Model modelo){
